b45062e1f2a271d5612a274a211e8c14569b65bf
Philipp <philipp@lueck-de.com>
2018-07-11 23:50:13 +0200

    GUI und DB beginn

--- source/view.php
+++ source/view.php
@@ -15,121 +15,127 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Prints a particular instance of ausleihverwaltung
+ * Prints a particular instance of ausleihantrag
  *
  * You can have a rather longer description of the file as well,
  * if you like, and it can span multiple lines.
  *
- * @package    mod_ausleihverwaltung
+ * @package    mod_ausleihantrag
  * @copyright  2016 Your Name <your@email.address>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Replace ausleihverwaltung with the name of your module and remove this line.
+// Replace ausleihantrag with the name of your module and remove this line.
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
 require_once(dirname(__FILE__).'/locallib.php');
 
 $id = optional_param('id', 0, PARAM_INT); // Course_module ID, or
-$n  = optional_param('n', 0, PARAM_INT);  // ... ausleihverwaltung instance ID - it should be named as the first character of the module.
+$n  = optional_param('n', 0, PARAM_INT);  // ... ausleihantrag instance ID - it should be named as the first character of the module.
 
 if ($id) {
-    $cm         = get_coursemodule_from_id('ausleihverwaltung', $id, 0, false, MUST_EXIST);
+    $cm         = get_coursemodule_from_id('ausleihantrag', $id, 0, false, MUST_EXIST);
     $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-    $ausleihverwaltung  = $DB->get_record('ausleihverwaltung', array('id' => $cm->instance), '*', MUST_EXIST);
+    $ausleihantrag  = $DB->get_record('ausleihantrag', array('id' => $cm->instance), '*', MUST_EXIST);
 } else if ($n) {
-    $ausleihverwaltung  = $DB->get_record('ausleihverwaltung', array('id' => $n), '*', MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $ausleihverwaltung->course), '*', MUST_EXIST);
-    $cm         = get_coursemodule_from_instance('ausleihverwaltung', $ausleihverwaltung->id, $course->id, false, MUST_EXIST);
+    $ausleihantrag  = $DB->get_record('ausleihantrag', array('id' => $n), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $ausleihantrag->course), '*', MUST_EXIST);
+    $cm         = get_coursemodule_from_instance('ausleihantrag', $ausleihantrag->id, $course->id, false, MUST_EXIST);
 } else {
     error('You must specify a course_module ID or an instance ID');
 }
 
 require_login($course, true, $cm);
 
-$event = \mod_ausleihverwaltung\event\course_module_viewed::create(array(
+$event = \mod_ausleihantrag\event\course_module_viewed::create(array(
     'objectid' => $PAGE->cm->instance,
     'context' => $PAGE->context,
 ));
 $event->add_record_snapshot('course', $PAGE->course);
-$event->add_record_snapshot($PAGE->cm->modname, $ausleihverwaltung);
+$event->add_record_snapshot($PAGE->cm->modname, $ausleihantrag);
 $event->trigger();
 
+// Print the page header.
 
-// Um Tabelle >>resources<< zu belegen
-/*
-$record = new stdClass();
-$record->name         = 'handy';
-$record->description = 'dasd';
-$record->serialnumber        = 'serial12';
-$record->inventorynumber = 'invent123';
-$record->comment        = 'Comment this Comment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment this';
-$record->status = 0;
-$record->amount         = 2;
-$record->type = 0;
-$record->maincategory    = "Handy";
-$record->subcategory = "sub";
-$record->defect = "damage is done";
-
-$DB->insert_record('ausleihverwaltung_resources', $record, $returnid=false, $bulk=false);
-
-/*
-$record1->name         = 'iPhone';
-$record1->description = 'beschde';
-$record1->serialnumber        = 'serial14';
-$record1->inventorynumber = 'invent567';
-$record1->comment        = 'Comment that';
-$record1->status = 3;
-$record1->amount         = 4;
-$record1->type = 1;
-$record1->maincategory    = "Apple";
-$record1->subcategory = "phone";
-$DB->insert_record('ausleihverwaltung_resources', $record1, $returnid=false, $bulk=false);
-
-$record2->name         = 'Mein iPhone';
-$record2->description = 'beschde';
-$record2->serialnumber        = 'blablub';
-$record2->inventorynumber = 'invent567';
-$record2->comment        = 'Comment that';
-$record2->status = 3;
-$record2->amount         = 4;
-$record2->type = 1;
-$record2->maincategory    = "Apple";
-$record2->subcategory = "phone";
-$DB->insert_record('ausleihverwaltung_resources', $record1, $returnid=false, $bulk=false);
-*/
-
-/* PAGE belegen*/
-$PAGE->set_url('/mod/ausleihverwaltung/view.php', array('id' => $cm->id));
-$PAGE->set_title(format_string($ausleihverwaltung->name));
+$PAGE->set_url('/mod/ausleihantrag/view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($ausleihantrag->name));
 $PAGE->set_heading(format_string($course->fullname));
 
 /*
  * Other things you may want to set - remove if not needed.
  * $PAGE->set_cacheable(false);
  * $PAGE->set_focuscontrol('some-html-id');
- * $PAGE->add_body_class('ausleihverwaltung-'.$somevar);
+ * $PAGE->add_body_class('ausleihantrag-'.$somevar);
  */
 
-// Hier beginnt die Ausgabe
+// Output starts here.
 echo $OUTPUT->header();
 
 // Conditions to show the intro can change to look for own settings or whatever.
-if ($ausleihverwaltung->intro) {
-    echo $OUTPUT->box(format_module_intro('ausleihverwaltung', $ausleihverwaltung, $cm->id), 'generalbox mod_introbox', 'ausleihverwaltungintro');
+if ($ausleihantrag->intro) {
+    echo $OUTPUT->box(format_module_intro('ausleihantrag', $ausleihantrag, $cm->id), 'generalbox mod_introbox', 'ausleihantragintro');
 }
 
-$strName = "Ressourcen-Übersicht";
-echo $OUTPUT->heading($strName);
 
-$attributes = array();
+
+// Replace the following lines with you own code.
+echo $OUTPUT->heading('Kategorie der Ressource auswählen.');
+
+
+// Implement form for user
+require_once(dirname(__FILE__).'/forms/simpleformNeu.php');
+
+$mform = new simplehtml_form();
+// $mform->render();
+
+// error_log("TEST FROM BEFORE DISPLAY");
+
+//Form processing and displaying is done here
+if ($mform->is_cancelled()) {
+    //Handle form cancel operation, if cancel button is present on form
+} else if ($fromform = $mform->get_data()) {
+    $value1 = $fromform->email;
+    $value2 = $fromform->name;
+
+    echo $value1;
+    error_log($value1);
+
+  //In this case you process validated data. $mform->get_data() returns data posted in form.
+  //Creating instance of relevant API modules
+  create_api_instances();
+  $process_definition_id = ausleihantrag_get_process_definition_id("testttest");
+  error_log("PROCESS DEFINITION ID IS: " . $process_definition_id);
+  $process_instance_id = ausleihantrag_start_process($process_definition_id, "test_key");
+  error_log("PROCESS INSTANCE ID IS: " . $process_instance_id);
+  sleep(2);
+  error_log("WAKEY WAKEY, BOYS AND GIRLS");
+  $taskid = ausleihantrag_check_for_input_required($process_instance_id);
+  error_log("TASK ID IS: " . $taskid);
+  if ($taskid != null) {
+    error_log("EXECUTION OF TASK RESPONSE");
+    $value1 = $fromform->email;
+    $value2 = $fromform->name;
+    $result = ausleihantrag_answer_input_required($taskid, $process_definition_id, $value1, $value2);
+    error_log("INPUT SEND RESULT IS: " . $result);
+  }
+} else {
+  // this branch is executed if the form is submitted but the data doesn't validate and the form should be redisplayed
+  // or on the first display of the form.
+
+  // Set default data (if any)
+  // Required for module not to crash as a course id is always needed
+  $formdata = array('id' => $id);
+  $mform->set_data($formdata);
+  //displays the form
+  $mform->display();
+
+  error_log("TEST FROM AFTER DISPLAY");
+}
 // Alle Datensätze aus der DB-Tabelle >>resources<< abfragen.
-$resource = $DB->get_records('ausleihverwaltung_resources');
-
+$resource = $DB->get_records('resources');
 $table = new html_table();
-$table->head = array('ID','Name', 'Beschreibung', 'Seriennummer', 'Inventarnummer', 'Kommentar', 'Status', 'Menge', 'Typ', 'Hauptkategorie', 'Subkategorie', 'Schaden', 'Bearbeiten', 'Löschen');
-
+$table->head = array('ID','Name', 'Description', 'Serialnumber', 'Inventorynumber', 'Comment', 'Status', 'Amount', 'Type', 'Maincategory', 'Subcategory');
 //Für jeden Datensatz
 foreach ($resource as $res) {
 $id = $res->id;
@@ -143,13 +149,39 @@ $amount = $res->amount;
 $type = $res->type;
 $maincategory = $res->maincategory;
 $subcategory = $res->subcategory;
-$defect = $res->defect;
 //Link zum Bearbeiten der aktuellen Ressource in foreach-Schleife setzen
-$htmlLink = html_writer::link(new moodle_url('../ausleihverwaltung/edit.php', array('id' => $cm->id, 'resourceid' => $res->id)), 'Edit', $attributes=null);
+
 //Analog: Link zum Löschen...
-$htmlLinkDelete = html_writer::link(new moodle_url('../ausleihverwaltung/delete.php', array('id' => $cm->id, 'resourceid' => $res->id)), 'Delete', $attributes=null);
+
 //Daten zuweisen an HTML-Tabelle
-$table->data[] = array($id, $name, $description, $serialnumber, $inventorynumber, $comment, $status, $amount, $type, $maincategory, $subcategory, $defect, $htmlLink, $htmlLinkDelete);
+$table->data[] = array($id, $name, $description, $serialnumber, $inventorynumber, $comment, $status, $amount, $type, $maincategory, $subcategory);
+}
+//Tabelle ausgeben
+echo html_writer::table($table);
+
+// Alle Datensätze aus der DB-Tabelle >>resources<< abfragen.
+$resource = $DB->get_records('apeinsvier_resources');
+$table = new html_table();
+$table->head = array('ID','Name', 'Description', 'Serialnumber', 'Inventorynumber', 'Comment', 'Status', 'Amount', 'Type', 'Maincategory', 'Subcategory');
+//Für jeden Datensatz
+foreach ($resource as $res) {
+$id = $res->id;
+$name = $res->name;
+$description = $res->description;
+$serialnumber = $res->serialnumber;
+$inventorynumber = $res->inventorynumber;
+$comment = $res->comment;
+$status = $res->status;
+$amount = $res->amount;
+$type = $res->type;
+$maincategory = $res->maincategory;
+$subcategory = $res->subcategory;
+//Link zum Bearbeiten der aktuellen Ressource in foreach-Schleife setzen
+$htmlLink = html_writer::link(new moodle_url('../apeinsvier/edit.php', array('id' => $cm->id, 'resourceid' => $res->id)));
+//Analog: Link zum Löschen...
+$htmlLinkDelete = html_writer::link(new moodle_url('../apeinsvier/delete.php', array('id' => $cm->id, 'resourceid' => $res->id)));
+//Daten zuweisen an HTML-Tabelle
+$table->data[] = array($id, $name, $description, $serialnumber, $inventorynumber, $comment, $status, $amount, $type, $maincategory, $subcategory, $htmlLink, $htmlLinkDelete);
 }
 //Tabelle ausgeben
 echo html_writer::table($table);
