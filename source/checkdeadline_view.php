b145397d2f1da50ff62c159a5afe7e008ed1b1f6
Sven <sbp.programming@gmail.com>
2018-07-12 21:24:37 +0200

    auslagerung von view elementen in ein eigenes php file

---
+++ source/checkdeadline_view.php
@@ -0,0 +1,208 @@
+<?php
+/*LOGIN*/
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+require_once(dirname(__FILE__).'/locallib.php');
+
+$id = optional_param('id', 0, PARAM_INT); // Course_module ID, or
+$n  = optional_param('n', 0, PARAM_INT);  // ... checkdeadline instance ID - it should be named as the first character of the module.
+
+if ($id) {
+    $cm         = get_coursemodule_from_id('checkdeadline', $id, 0, false, MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $checkdeadline  = $DB->get_record('checkdeadline', array('id' => $cm->instance), '*', MUST_EXIST);
+} else if ($n) {
+    $checkdeadline  = $DB->get_record('checkdeadline', array('id' => $n), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $checkdeadline->course), '*', MUST_EXIST);
+    $cm         = get_coursemodule_from_instance('checkdeadline', $checkdeadline->id, $course->id, false, MUST_EXIST);
+} else {
+    error('You must specify a course_module ID or an instance ID');
+}
+
+require_login($course, true, $cm);
+
+$event = \mod_checkdeadline\event\course_module_viewed::create(array(
+    'objectid' => $PAGE->cm->instance,
+    'context' => $PAGE->context,
+));
+$event->add_record_snapshot('course', $PAGE->course);
+$event->add_record_snapshot($PAGE->cm->modname, $checkdeadline);
+$event->trigger();
+
+/*PAGE setzen*/
+$PAGE->set_url('/mod/checkdeadline/checkdeadline_view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($checkdeadline->name));
+$PAGE->set_heading(format_string($course->fullname));
+
+
+/*
+// Um Tabelle >>borroweddevices<< zu belegen
+$record1->duedate = 1530897520;
+$record1->inventorynumber = '09876';
+$record1->studentmatrikelnummer = 3156763;
+$record1->studentmailaddress = 's153640@student.dhbw-mannheim.de';
+
+$DB->insert_record('borroweddevices', $record1, $returnid=false, $bulk=false);
+
+$record1->duedate = 1530859235;
+$record1->inventorynumber = '73463574';
+$record1->studentmatrikelnummer = 8528640;
+$record1->studentmailaddress = 's145634@student.dhbw-mannheim.de';
+
+$DB->insert_record('borroweddevices', $record1, $returnid=false, $bulk=false);
+
+$record1->duedate = 1532500835;
+$record1->inventorynumber = '134255';
+$record1->studentmatrikelnummer = 1630452;
+$record1->studentmailaddress = 's153660@student.dhbw-mannheim.de';
+
+$DB->insert_record('borroweddevices', $record1, $returnid=false, $bulk=false);
+
+$record1->duedate = 1524638435;
+$record1->inventorynumber = '56765';
+$record1->studentmatrikelnummer = 5654193;
+$record1->studentmailaddress = 's153670@student.dhbw-mannheim.de';
+
+$DB->insert_record('borroweddevices', $record1, $returnid=false, $bulk=false);
+
+// Datensatz mit übergebener ID löschen
+//$DB->delete_records_select('borroweddevices',"id = 2", $params=null);
+
+
+// Um Tabelle >>responsibledudes<< zu belegen
+$record1->dudesname = 'Mister Meister';
+$record1->dudesmail = 'meistermeister@dude.com';
+
+$DB->insert_record('responsibledudes', $record1, $returnid=false, $bulk=false);
+
+$record1->dudesname = 'Mister Martin';
+$record1->dudesmail = 'clemens.martin@dude.com';
+
+$DB->insert_record('responsibledudes', $record1, $returnid=false, $bulk=false);
+
+$record1->dudesname = 'Miss Frohwein';
+$record1->dudesmail = 'miss.frohwein@dude.com';
+
+$DB->insert_record('responsibledudes', $record1, $returnid=false, $bulk=false);
+*/
+
+
+// Hier beginnt die Ausgabe
+echo $OUTPUT->header();
+
+// Conditions to show the intro can change to look for own settings or whatever.
+if ($checkdeadline->intro) {
+    echo $OUTPUT->box(format_module_intro('checkdeadline', $checkdeadline, $cm->id), 'generalbox mod_introbox', 'checkdeadlineintro');
+}
+
+$strName = "Ausleihen-Übersicht";
+echo $OUTPUT->heading($strName);
+
+// Alle Datensätze aus der DB-Tabelle >>$borroweddevices<< abfragen.
+$borrowed = $DB->get_records('borroweddevices');
+
+$table = new html_table();
+$table->head = array('ID','Due Date', 'Inventarnummer', 'Matrikelnummer', 'Studenten E-Mail');
+
+//Für jeden Datensatz
+foreach ($borrowed as $borrowed) {
+    $rowId = $borrowed->id;
+    $duedate = $borrowed->duedate;
+    $inventorynumber = $borrowed->inventorynumber;
+    $studentmatrikelnummer = $borrowed->studentmatrikelnummer;
+    $studentmailaddress = $borrowed->studentmailaddress;
+//Daten zuweisen an HTML-Tabelle
+    $table->data[] = array($rowId, $duedate, $inventorynumber, $studentmatrikelnummer, $studentmailaddress);
+}
+//Tabelle ausgeben
+echo html_writer::table($table);
+
+
+$strName = "Verantwortlichen-Übersicht";
+echo $OUTPUT->heading($strName);
+
+// Alle Datensätze aus der DB-Tabelle >>responsibledudes<< abfragen.
+$responsibleDudes = $DB->get_records('responsibledudes');
+
+$table = new html_table();
+$table->head = array('Name des Verantwortlichen', 'E-Mail des Verantwortlichen', 'Löschen');
+
+//Für jeden Datensatz
+foreach ($responsibleDudes as $responsible) {
+    $name = $responsible->dudesname;
+    $mail = $responsible->dudesmail;
+    //Link zum löschen des Verantwortlichen in foreach-Schleife setzen
+    $htmlLinkDelete = html_writer::link(new moodle_url('../checkdeadline/checkdeadline_delete.php', array('id' => $cm->id, 'responsibleid' => $responsible->id)), 'Delete', $attributes=null);
+//Daten zuweisen an HTML-Tabelle
+    $table->data[] = array($name, $mail, $htmlLinkDelete);
+}
+//Tabelle ausgeben
+echo html_writer::table($table);
+
+
+$strName = "Gibt es weitere Verantwortliche?";
+echo $OUTPUT->heading($strName);
+
+// Implement form for user
+require_once(dirname(__FILE__).'/forms/checkDeadlineControllForm.php');
+//require_once(dirname(__FILE__).'/forms/leihscheinForm.php');
+
+$mform = new checkDeadlineControllForm_form();
+//$mform = new leihschein_form();
+// $mform->render();
+
+// Form processing and displaying is done here
+if ($mform->is_cancelled()) {
+    // Handle form cancel operation, if cancel button is present on form
+    echo "form is cancelled";
+} else if ($fromform = $mform->get_data()) {
+
+    $dudesName = $fromform->responsibleName;
+    $dudesMail = $fromform->responsibleMail;
+
+// Um Tabelle >>responsibledudes<< zu belegen
+    $record1 = new \stdClass();
+    $record1->dudesname = $dudesName;
+    $record1->dudesmail = $dudesMail;
+    $DB->insert_record('responsibledudes', $record1, $returnid=false, $bulk=false);
+
+    //reload page so all Table views will be updated and Forms will be redisplayed
+    redirect(new moodle_url('../checkdeadline/view.php', array('id' => $cm->id)));
+
+// $mform->render();
+
+    // In this case you process validated data. $mform->get_data() returns data posted in form.
+    // Creating instance of relevant API modules
+    create_api_instances();
+    $process_definition_id = checkdeadline_get_process_definition_id("testttest");
+    error_log("PROCESS DEFINITION ID IS: " . $process_definition_id);
+    //$process_instance_id = checkdeadline_start_process($process_definition_id, "test_key");
+    //error_log("PROCESS INSTANCE ID IS: " . $process_instance_id);
+    sleep(2);
+    error_log("WAKEY WAKEY, BOYS AND GIRLS");
+    //$taskid = checkdeadline_check_for_input_required($process_instance_id);
+    /*error_log("TASK ID IS: " . $taskid);
+    if ($taskid != null) {
+        error_log("EXECUTION OF TASK RESPONSE");
+        $value1 = $fromform->email;
+        $value2 = $fromform->name;
+        $borrowedult = checkdeadline_answer_input_required($task_id, $process_definition_id, $value1, $value2);
+        error_log("INPUT SEND RESULT IS: " . $borrowedult);
+    }*/
+} else {
+    // this branch is executed if the form is submitted but the data doesn't validate and the form should be redisplayed
+    // or on the first display of the form.
+
+    // Set default data (if any)
+    // Required for module not to crash as a course id is always needed
+    $formdata = array('id' => $id);
+    $mform->set_data($formdata);
+    // displays the form
+    $mform->display();
+
+    error_log("TEST FROM AFTER DISPLAY");
+}
+
+//FINISH
+echo $OUTPUT->footer();
+?>
--- source/view.php
+++ source/view.php
@@ -25,6 +25,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+// Replace checkdeadline with the name of your module and remove this line.
+
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
 require_once(dirname(__FILE__).'/locallib.php');
@@ -55,60 +57,50 @@ $event->add_record_snapshot($PAGE->cm->modname, $checkdeadline);
 $event->trigger();
 
 
+// Um Tabelle >>resources<< zu belegen
 /*
-// Um Tabelle >>borroweddevices<< zu belegen
-$record1->duedate = 1530897520;
-$record1->inventorynumber = '09876';
-$record1->studentmatrikelnummer = 3156763;
-$record1->studentmailaddress = 's153640@student.dhbw-mannheim.de';
+$record = new stdClass();
+$record->name         = 'handy';
+$record->description = 'dasd';
+$record->serialnumber        = 'serial12';
+$record->inventorynumber = 'invent123';
+$record->comment        = 'Comment this Comment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment thisComment this';
+$record->status = 0;
+$record->amount         = 2;
+$record->type = 0;
+$record->maincategory    = "Handy";
+$record->subcategory = "sub";
+$record->defect = "damage is done";
 
-$DB->insert_record('borroweddevices', $record1, $returnid=false, $bulk=false);
+$DB->insert_record('checkdeadline_resources', $record, $returnid=false, $bulk=false);
 
-$record1->duedate = 1530859235;
-$record1->inventorynumber = '73463574';
-$record1->studentmatrikelnummer = 8528640;
-$record1->studentmailaddress = 's145634@student.dhbw-mannheim.de';
+/*
+$record1->name         = 'iPhone';
+$record1->description = 'beschde';
+$record1->serialnumber        = 'serial14';
+$record1->inventorynumber = 'invent567';
+$record1->comment        = 'Comment that';
+$record1->status = 3;
+$record1->amount         = 4;
+$record1->type = 1;
+$record1->maincategory    = "Apple";
+$record1->subcategory = "phone";
+$DB->insert_record('checkdeadline_resources', $record1, $returnid=false, $bulk=false);
 
-$DB->insert_record('borroweddevices', $record1, $returnid=false, $bulk=false);
-
-$record1->duedate = 1532500835;
-$record1->inventorynumber = '134255';
-$record1->studentmatrikelnummer = 1630452;
-$record1->studentmailaddress = 's153660@student.dhbw-mannheim.de';
-
-$DB->insert_record('borroweddevices', $record1, $returnid=false, $bulk=false);
-
-$record1->duedate = 1524638435;
-$record1->inventorynumber = '56765';
-$record1->studentmatrikelnummer = 5654193;
-$record1->studentmailaddress = 's153670@student.dhbw-mannheim.de';
-
-$DB->insert_record('borroweddevices', $record1, $returnid=false, $bulk=false);
-
-// Datensatz mit übergebener ID löschen
-//$DB->delete_records_select('borroweddevices',"id = 2", $params=null);
-
-
-// Um Tabelle >>responsibledudes<< zu belegen
-$record1->dudesname = 'Mister Meister';
-$record1->dudesmail = 'meistermeister@dude.com';
-
-$DB->insert_record('responsibledudes', $record1, $returnid=false, $bulk=false);
-
-$record1->dudesname = 'Mister Martin';
-$record1->dudesmail = 'clemens.martin@dude.com';
-
-$DB->insert_record('responsibledudes', $record1, $returnid=false, $bulk=false);
-
-$record1->dudesname = 'Miss Frohwein';
-$record1->dudesmail = 'miss.frohwein@dude.com';
-
-$DB->insert_record('responsibledudes', $record1, $returnid=false, $bulk=false);
+$record2->name         = 'Mein iPhone';
+$record2->description = 'beschde';
+$record2->serialnumber        = 'blablub';
+$record2->inventorynumber = 'invent567';
+$record2->comment        = 'Comment that';
+$record2->status = 3;
+$record2->amount         = 4;
+$record2->type = 1;
+$record2->maincategory    = "Apple";
+$record2->subcategory = "phone";
+$DB->insert_record('checkdeadline_resources', $record1, $returnid=false, $bulk=false);
 */
 
-
-// Print the page header.
-
+/* PAGE belegen*/
 $PAGE->set_url('/mod/checkdeadline/view.php', array('id' => $cm->id));
 $PAGE->set_title(format_string($checkdeadline->name));
 $PAGE->set_heading(format_string($course->fullname));
@@ -120,7 +112,7 @@ $PAGE->set_heading(format_string($course->fullname));
  * $PAGE->add_body_class('checkdeadline-'.$somevar);
  */
 
-// Output starts here.
+// Hier beginnt die Ausgabe
 echo $OUTPUT->header();
 
 // Conditions to show the intro can change to look for own settings or whatever.
@@ -131,110 +123,41 @@ if ($checkdeadline->intro) {
 $strName = "Ausleihen-Übersicht";
 echo $OUTPUT->heading($strName);
 
-// Alle Datensätze aus der DB-Tabelle >>$borroweddevices<< abfragen.
-$borrowed = $DB->get_records('borroweddevices');
+echo $OUTPUT->single_button(new moodle_url('../checkdeadline/checkdeadline_view.php', array('id' => $cm->id)), 'Ausleihübersicht anzeigen');
+
+$strName = "Ressourcen-Übersicht";
+echo $OUTPUT->heading($strName);
+
+$attributes = array();
+// Alle Datensätze aus der DB-Tabelle >>resources<< abfragen.
+$resource = $DB->get_records('checkdeadline_resources');
 
 $table = new html_table();
-$table->head = array('ID','Due Date', 'Inventarnummer', 'Matrikelnummer', 'Studenten E-Mail');
+$table->head = array('ID','Name', 'Beschreibung', 'Seriennummer', 'Inventarnummer', 'Kommentar', 'Status', 'Menge', 'Typ', 'Hauptkategorie', 'Subkategorie', 'Schaden', 'Bearbeiten', 'Löschen');
 
 //Für jeden Datensatz
-foreach ($borrowed as $borrowed) {
-    $rowId = $borrowed->id;
-    $duedate = $borrowed->duedate;
-    $inventorynumber = $borrowed->inventorynumber;
-    $studentmatrikelnummer = $borrowed->studentmatrikelnummer;
-    $studentmailaddress = $borrowed->studentmailaddress;
+foreach ($resource as $res) {
+    $id = $res->id;
+    $name = $res->name;
+    $description = $res->description;
+    $serialnumber = $res->serialnumber;
+    $inventorynumber = $res->inventorynumber;
+    $comment = $res->comment;
+    $status = $res->status;
+    $amount = $res->amount;
+    $type = $res->type;
+    $maincategory = $res->maincategory;
+    $subcategory = $res->subcategory;
+    $defect = $res->defect;
+//Link zum Bearbeiten der aktuellen Ressource in foreach-Schleife setzen
+    $htmlLink = html_writer::link(new moodle_url('../checkdeadline/edit.php', array('id' => $cm->id, 'resourceid' => $res->id)), 'Edit', $attributes=null);
+//Analog: Link zum Löschen...
+    $htmlLinkDelete = html_writer::link(new moodle_url('../checkdeadline/delete.php', array('id' => $cm->id, 'resourceid' => $res->id)), 'Delete', $attributes=null);
 //Daten zuweisen an HTML-Tabelle
-    $table->data[] = array($rowId, $duedate, $inventorynumber, $studentmatrikelnummer, $studentmailaddress);
+    $table->data[] = array($id, $name, $description, $serialnumber, $inventorynumber, $comment, $status, $amount, $type, $maincategory, $subcategory, $defect, $htmlLink, $htmlLinkDelete);
 }
 //Tabelle ausgeben
 echo html_writer::table($table);
-
-
-$strName = "Verantwortlichen-Übersicht";
-echo $OUTPUT->heading($strName);
-
-// Alle Datensätze aus der DB-Tabelle >>responsibledudes<< abfragen.
-$responsibleDudes = $DB->get_records('responsibledudes');
-
-$table = new html_table();
-$table->head = array('Name des Verantwortlichen', 'E-Mail des Verantwortlichen', 'Löschen');
-
-//Für jeden Datensatz
-foreach ($responsibleDudes as $responsible) {
-    $name = $responsible->dudesname;
-    $mail = $responsible->dudesmail;
-    //Link zum löschen des Verantwortlichen in foreach-Schleife setzen
-    $htmlLinkDelete = html_writer::link(new moodle_url('../checkdeadline/checkdeadline_delete.php', array('id' => $cm->id, 'responsibleid' => $responsible->id)), 'Delete', $attributes=null);
-//Daten zuweisen an HTML-Tabelle
-    $table->data[] = array($name, $mail, $htmlLinkDelete);
-}
-//Tabelle ausgeben
-echo html_writer::table($table);
-
-
-$strName = "Gibt es weitere Verantwortliche?";
-echo $OUTPUT->heading($strName);
-
-// Implement form for user
-require_once(dirname(__FILE__).'/forms/checkDeadlineControllForm.php');
-//require_once(dirname(__FILE__).'/forms/leihscheinForm.php');
-
-$mform = new checkDeadlineControllForm_form();
-//$mform = new leihschein_form();
-// $mform->render();
-
-// Form processing and displaying is done here
-if ($mform->is_cancelled()) {
-    // Handle form cancel operation, if cancel button is present on form
-    echo "form is cancelled";
-} else if ($fromform = $mform->get_data()) {
-
-    $dudesName = $fromform->responsibleName;
-    $dudesMail = $fromform->responsibleMail;
-
-// Um Tabelle >>responsibledudes<< zu belegen
-    $record1 = new \stdClass();
-    $record1->dudesname = $dudesName;
-    $record1->dudesmail = $dudesMail;
-    $DB->insert_record('responsibledudes', $record1, $returnid=false, $bulk=false);
-
-    //reload page so all Table views will be updated and Forms will be redisplayed
-    redirect(new moodle_url('../checkdeadline/view.php', array('id' => $cm->id)));
-
-// $mform->render();
-
-    // In this case you process validated data. $mform->get_data() returns data posted in form.
-    // Creating instance of relevant API modules
-    create_api_instances();
-    $process_definition_id = checkdeadline_get_process_definition_id("testttest");
-    error_log("PROCESS DEFINITION ID IS: " . $process_definition_id);
-    //$process_instance_id = checkdeadline_start_process($process_definition_id, "test_key");
-    //error_log("PROCESS INSTANCE ID IS: " . $process_instance_id);
-    sleep(2);
-    error_log("WAKEY WAKEY, BOYS AND GIRLS");
-    //$taskid = checkdeadline_check_for_input_required($process_instance_id);
-    /*error_log("TASK ID IS: " . $taskid);
-    if ($taskid != null) {
-        error_log("EXECUTION OF TASK RESPONSE");
-        $value1 = $fromform->email;
-        $value2 = $fromform->name;
-        $borrowedult = checkdeadline_answer_input_required($task_id, $process_definition_id, $value1, $value2);
-        error_log("INPUT SEND RESULT IS: " . $borrowedult);
-    }*/
-} else {
-    // this branch is executed if the form is submitted but the data doesn't validate and the form should be redisplayed
-    // or on the first display of the form.
-
-    // Set default data (if any)
-    // Required for module not to crash as a course id is always needed
-    $formdata = array('id' => $id);
-    $mform->set_data($formdata);
-    // displays the form
-    $mform->display();
-
-    error_log("TEST FROM AFTER DISPLAY");
-}
 
 // Finish the page.
 echo $OUTPUT->footer();
