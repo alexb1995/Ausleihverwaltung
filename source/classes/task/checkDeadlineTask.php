20b7d56cc03b1115a792b7a13c4db8f73a096727
Sven <sbp.programming@gmail.com>
2018-07-15 20:20:20 +0200

    Update des Checkdeadline Scheduled Task
    - Mailtexte wurden überarbeitet
    - unnötige Message Klassennutzung entfernt
    - Tabelle der Verantwortlichen zur Benachrichtigung abgerufen
    - Check ob resource schon wieder Abgegeben wurde. -> nur Mail, wenn noch nicht abgegeben (natürlich)

--- source/classes/task/checkDeadlineTask.php
+++ source/classes/task/checkDeadlineTask.php
@@ -5,7 +5,7 @@
  * Time: 13:58
  */
 
-namespace mod_ausleihverwaltung\task;
+namespace mod_checkdeadline\task;
 use \DateTime;
 
 //require_once(dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/lib/classes/date.php');
@@ -16,59 +16,58 @@ require_once(dirname(dirname(dirname(__FILE__))).'/locallib.php');
 class checkDeadlineTask extends \core\task\scheduled_task {
     public function get_name() {
         // Shown in admin screens
-        return 'ausleihverwaltung';
+        return 'Überprüfung auf resourcen in Verzug';
     }
 
     public function execute() {
         global $DB;
         global $CFG;
-        global $responsibleMail;
-        $supportUser = $DB->get_record('user', array('username' => 'supportuser')); //the message is sent from a bot user "Support User"
-        $receivingUser = $DB->get_record('user', array('username' => 'user')); //the message is sent from a bot user "Support User"
-        $course = $DB->get_record('course', array('shortname' => 'testcourse')); //the message is sent from a bot user "Support User"
-        //message creation
-        $message = new \core\message\message();
-        $message->component = 'moodle';
-        $message->name = 'instantmessage';
-        $message->userfrom = $supportUser;
-        $message->userto = $receivingUser;
-        $message->subject = 'Check Deadline';
-        $message->fullmessageformat = FORMAT_MARKDOWN;
-        $message->fullmessagehtml = '<p>message body</p>';
-        $message->notification = '0';
-        $message->contexturl = '';
-        $message->contexturlname = 'Context name';
-        $message->replyto = "";
-        $content = array('*' => array('header' => ' test ', 'footer' => ' test ')); // Extra content for specific processor
-        $message->set_additional_content('email', $content);
-        $message->courseid = $course->id; // This is required in recent versions, use it from 3.2 on https://tracker.moodle.org/browse/MDL-47162
-        /*$message->fullmessage = 'ausleihverwaltung task works';
-        $message->smallmessage = 'ausleihverwaltung task works';             
-        $messageid = message_send($message);*/
-
-        //$coreDateObject = new \core\core_date\core_date();
 
         // Alle Datensätze aus der DB-Tabelle >>$ausleihverwaltung_borroweddevice<< abfragen.
-        $borrowed = $DB->get_records('ausleihverwaltung_borroweddevice');
-        //Aktuelle GMT Systemzeit im Epoch Time Format (Sekunden seit 1. Januar 1970) Deutschland ist GMT plus 2
-        //$now = new DateTime("now", $coreDateObject);
-        /*
-         * Normalerweise sollte die Systemzeit mit
-         */
+        $borrowed = $DB->get_records('checkdeadline_borroweddevice');
+        $responsible = $DB->get_records('checkdeadline_responsible');
+
+        //Aktuelle GMT Systemzeit im Epoch Time Format (Sekunden seit 1. Januar 1970). Deutschland ist GMT plus 2
         $now = new DateTime();
         $now = $now->getTimestamp();
         $yesterday = $now - 129600;
 
         foreach ($borrowed as $borrowed) {
-            $duedate = $borrowed->duedate;
-            $inventorynumber = $borrowed->inventorynumber;
-            $studentmatrikelnummer = $borrowed->studentmatrikelnummer;
-            $studentmailaddress = $borrowed->studentmailaddress;
-            if ($duedate < $now ){ //&& $duedate > $yesterday
-                $mailText = "Das Gerät mit der Inventarnummer " . $inventorynumber . ", welches vom 
-                Studenten mit der Matrikelnummer " . $studentmatrikelnummer . " und der E-Mail-Adresse " .
-                    $studentmailaddress . " ausgeliehen wurde ist überfällig.";
-                mail_to('chs.padutsch@gmail.com', 'Sven', 'Überfälliges Device', $mailText);
+            if ($borrowed->accepted && !($borrowed->returned)){
+                $duedate = $borrowed->duedate;
+                if ($duedate < $now && $duedate > $yesterday){
+                    $resourceId = $borrowed->resourceid;
+                    $studentmatrikelnummer = $borrowed->studentmatrikelnummer;
+                    $studentmailaddress = $borrowed->studentmailaddress;
+                    $studentName = $borrowed->studentname;
+
+                    $mailText = "Sehr geehrter " . $studentName . ",
+                    
+sie haben ein Gerät der DHBW Mannheim mit der ID " . $resourceId . " in ihrem Besitzt. Wir mussten leider feststellen, dass die Rückgabefrist überschritten wurde. 
+Wir möchten Sie daher bitten, die ausgeliehene resource schnellstmöglich an die verantwortliche Person zurückzugeben. 
+
+Dies ist eine automatisch generierte Mail. Antworten auf diese Mail werden daher unbeantwortet bleiben.
+
+Mit freundlichm Gruß, 
+das IT Team der DHBW Mannheim";
+
+                    mail_to($studentmailaddress, $studentName, 'Überfälliges Device', $mailText);
+
+                    foreach ($responsible as $res) {
+                        $responsibleMail = $res->dudesmail;
+                        $responsibleName = $res->dudesname;
+                        $mailText = "Sehr geehrter ". $responsibleName . ",
+Das Gerät mit der Geräte ID " . $resourceId . ", welches vom Studenten " . $studentName . " mit der Matrikelnummer " . $studentmatrikelnummer . " und der E-Mail-Adresse " . $studentmailaddress . " ausgeliehen wurde ist überfällig.
+Eine Erinnerung mit der Bitte zur schnellstmöglichen Rückgabe wurde verschickt. Falls Sie in den Nächsten Tagen keine Reaktion des Studierenden wahrnehmen, leiten Sie weitere Schritte gegen den Studierenden ein.
+
+Dies ist eine automatisch generierte Mail. Antworten auf diese Mail werden daher unbeantwortet bleiben.
+
+Mit freundlichm Gruß, 
+das IT Team der DHBW Mannheim";
+
+                        mail_to($responsibleMail, $responsibleName, 'Überfälliges Device', $mailText);
+                    }
+                }
             }
         }
     }
